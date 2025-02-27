cbdc0d56d6a764660541fd21fa74f1192e7f8c9e
Elvis Aron Presley Bail <elvisbail6@gmail.com>
2025-02-27 12:12:14 -0300

    Aula 03

---
+++ Aula03/aula03.js
@@ -0,0 +1,32 @@
+let n1 = 10  //number inteiro
+let n2 = 2   //number inteiro
+let n3 = '2' //string texto
+
+document.write(`N1 ${n1} <br>`)
+document.write(`N1 ${n2} <br>`)
+document.write(`N1 ${n3} <br>`)
+
+console.log(n1 + n2) // adicao
+console.log(n1 - n2) // subtracao
+console.log(n1 * n2) // multiplicacao
+console.log(n1 / n2) // divisao
+console.log(n1 % n2) // resto ou modulo
+
+console.log(n2 == n3)  // igual a
+console.log(n2 === n3) // identico a 
+console.log(n2 != n3)  // nao é igual
+console.log(n2 !== n3) // Nao é identico
+
+console.log(n1 > n2)   // MAIOR
+console.log(n1 < n2)   // MENOR
+console.log(n1 >= n2)  // MAIOR OU IGUAL
+console.log(n1 <= n2)  // MENOR OU IGUAL
+
+let a = true
+let b = false
+console.log(a && a) // E AND
+console.log(a || a) // OU OR
+console.log(!a)     // NAO NOT negacao
+console.log(!b) 
+
+/* LINK DA AULA : https://www.youtube.com/watch?v=6d6IV3SQnIM&list=PLnex8IkmReXxZEXje06kW1uCwm5iC8M_Z&index=3 */
